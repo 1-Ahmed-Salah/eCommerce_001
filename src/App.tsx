e9af0fd09994e33b145f225fb33f3df6a29c5d81
1-Ahmed-Salah <aysrahmed1@gmail.com>
2025-02-21 12:00:08 +0500

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+     <div className="container">
+      <h1 className="text-xl font-bold font-secound ">
+          Hello worldy!
+        </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
